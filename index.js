7ff1755ee9c6cf73528e52a9782d0bf9f8856b3c
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-05-01 16:31:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = 'token'
+fetch('https://api.github.com/lindsay-c-dennis/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
