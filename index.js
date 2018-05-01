be1119ad0af4b29cba42575e35266655b6008c44
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-05-01 16:31:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much.";
 
-const token = 'a3c07ffd26ec990a9df9fc5788d67d3f5067a387'
+const token = 'token'
 fetch('https://api.github.com/lindsay-c-dennis/repos', {
   headers: {
     Authorization: `token ${token}`
